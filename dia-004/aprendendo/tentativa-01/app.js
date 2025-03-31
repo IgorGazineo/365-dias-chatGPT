79c43251ae98e48401be0cdb4680dcf8a19fe7ad
Igor Gazio <gaumgazineo@hotmail.com>
2025-03-31 04:54:22 -0300

    finalizando a tentativa-01 da pasta 'aprendendo' do dia 04

---
+++ dia-004/aprendendo/tentativa-01/app.js
@@ -0,0 +1,5 @@
+var numeros = [1, 2, 3, 4, 5];
+var primeiroNumero = numeros[0];
+var ultimoNumero = numeros[numeros.length - 1];
+console.log(primeiroNumero);
+console.log(ultimoNumero);
---
+++ dia-004/aprendendo/tentativa-01/app.ts
@@ -0,0 +1,6 @@
+const numeros: number[] = [1, 2, 3, 4, 5];
+const primeiroNumero: number = numeros[0];
+const ultimoNumero: number = numeros[numeros.length - 1];
+
+console.log(primeiroNumero);
+console.log(ultimoNumero);
