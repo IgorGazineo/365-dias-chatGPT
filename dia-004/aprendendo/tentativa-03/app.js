d35e5723b6157dc109635e50709fe58a0206de7c
Igor Gazio <gaumgazineo@hotmail.com>
2025-03-31 07:09:18 -0300

    finalizando a tentativa-03 da pasta 'aprendendo' do dia 04

---
+++ dia-004/aprendendo/tentativa-03/app.js
@@ -0,0 +1,4 @@
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+for (var i = 0; i < numeros.length; i++) {
+    console.log(numeros[i]);
+}
---
+++ dia-004/aprendendo/tentativa-03/app.ts
@@ -0,0 +1,5 @@
+const numeros: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+
+for (let i: number = 0; i < numeros.length; i++) {
+  console.log(numeros[i]);
+}
