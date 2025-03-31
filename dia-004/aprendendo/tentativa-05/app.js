f45fe29bcff275d5103da03861a3ee89e29953bc
Igor Gazio <gaumgazineo@hotmail.com>
2025-03-31 08:19:17 -0300

    finalizando a tentativa-05 da pasta 'aprendendo' do dia 04

---
+++ dia-004/aprendendo/tentativa-05/app.js
@@ -0,0 +1,6 @@
+var numeros = [10, 20, 30, 40, 50];
+var soma = 0;
+for (var i = 0; i < numeros.length; i++) {
+    soma += numeros[i];
+}
+console.log(soma);
---
+++ dia-004/aprendendo/tentativa-05/app.ts
@@ -0,0 +1,9 @@
+const numeros: number[] = [10, 20, 30, 40, 50];
+
+let soma: number = 0;
+
+for (let i: number = 0; i < numeros.length; i++) {
+  soma += numeros[i];
+}
+
+console.log(soma);
